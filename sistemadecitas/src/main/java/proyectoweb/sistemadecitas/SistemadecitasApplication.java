da7973a297cbb095d308d767f9265c878eafaeb1
robinsonR18 <robinrc885@gmail.com>
2025-04-19 23:59:13 -0500

    avance proyecto 0.1

---
+++ sistemadecitas/src/main/java/proyectoweb/sistemadecitas/SistemadecitasApplication.java
@@ -0,0 +1,13 @@
+package proyectoweb.sistemadecitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadecitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadecitasApplication.class, args);
+	}
+
+}
---
+++ sistemadecitas/src/test/java/proyectoweb/sistemadecitas/SistemadecitasApplicationTests.java
@@ -0,0 +1,13 @@
+package proyectoweb.sistemadecitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadecitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
